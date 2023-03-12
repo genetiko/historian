478d8438809a6de6fbd3bbf51f439ec59338b8f2
Sasha K <a.kosak.mbx@gmail.com>
2023-03-12 20:37:53 +0300

    Plug rooter was added to handle `/` path. Issues fixed.

--- historian/main.py
+++ historian/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
-from historian.routers import history
-from historian.routers import symbols
+from historian.loader.router import router as loader_router
+from historian.provider.router import router as provider_router
 
 app = FastAPI()
 
-app.include_router(symbols.router)
-app.include_router(history.router)
+
+@app.get('/')
+async def main():
+    return JSONResponse(content={})
+
+
+app.include_router(provider_router)
+app.include_router(loader_router)
